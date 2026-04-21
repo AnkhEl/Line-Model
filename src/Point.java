18bbf5019566a33c7180e40689b3c95788fe38d7
Alex Khumalo <t.alex.khumalo@gmail.com>
2026-04-21 11:01:38 +0200

    Added Point class

---
+++ src/Point.java
@@ -0,0 +1,34 @@
+public class Point {
+	
+    private String name;
+    private double x;
+    private double y;
+
+    public Point(String name, double x, double y){
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+    
+    public double getX() {
+		return x;
+	}
+    
+    public double getY() {
+		return y;
+	}
+    
+    public void setX(double x) {
+		this.x = x;
+	}
+    
+    public void setY(double y) {
+		this.y = y;
+	}
+    
+    
+    @Override
+    public String toString() {
+    	return String.format("%s(%d,%d)",name,x,y);
+    }
+}
